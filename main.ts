9bc0a7c5411bf89fe1efc2d324d63c37df825ca7
Bram Adams <3282661+bramses@users.noreply.github.com>
2023-03-04 11:39:05 -0500

    init

--- main.ts
+++ main.ts
@@ -1,89 +1,239 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import {
+	App,
+	Editor,
+	MarkdownView,
+	Plugin,
+	PluginSettingTab,
+	Setting,
+	request
+} from "obsidian";
 
 // Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
-	mySetting: string;
+interface ChatGPT_MDSettings {
+	apiKey: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+const DEFAULT_SETTINGS: ChatGPT_MDSettings = {
+	apiKey: "default",
+};
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class ChatGPT_MD extends Plugin {
+	settings: ChatGPT_MDSettings;
+
+	async callOpenAIAPI(
+		editor: Editor,
+		messages: {role: string, content: string}[],
+		model = "gpt-3.5-turbo",
+		max_tokens = 250,
+		temperature = 0.3,
+		top_p = 1,
+		presence_penalty = 0.5,
+		frequency_penalty = 0.5,
+		stream = true,
+		stop = null,
+		n = 1,
+		logit_bias = null,
+		user = null
+	) {
+
+		console.log("calling openai api");
+		console.log(`args: ${JSON.stringify({
+			model: model,
+			messages: messages,
+			max_tokens: max_tokens,
+			temperature: temperature,
+			top_p: top_p,
+			presence_penalty: presence_penalty,
+			frequency_penalty: frequency_penalty,
+			stream: stream,
+			stop: stop,
+			n: n,
+			logit_bias: logit_bias,
+			user: user,
+		}, null, 2)}`)
+
+		// return "hello world";
+
+		const response = await request({
+			url: `https://api.openai.com/v1/chat/completions`,
+			method: "POST",
+			headers: {
+				Authorization: `Bearer ${this.settings.apiKey}`,
+				"Content-Type": "application/json",
+			},
+			contentType: "application/json",
+			body: JSON.stringify({
+				model: model,
+				messages: messages,
+				// max_tokens: max_tokens,
+				// temperature: temperature,
+				// top_p: top_p,
+				// presence_penalty: presence_penalty,
+				// frequency_penalty: frequency_penalty,
+				stream: stream,
+				// stop: stop,
+				// n: n,
+				// logit_bias: logit_bias,
+				// user: user,
+			}),
+		});
+
+		console.log(response);
+
+		if (stream) {
+			/*
+			write data to file as it comes in while data: [DONE] is not in response
+			data: {"id":"chatcmpl-6qNwACYUNL4z7HdFNTprBGONTYCa0","object":"chat.completion.chunk","created":1677943090,"model":"gpt-3.5-turbo-0301","choices":[{"delta":{"content":" converts"},"index":0,"finish_reason":null}]}
+			*/
+
+			// split response by new line
+			const responseLines = response.split("\n\n");
+
+			// remove data: from each line
+			for (let i = 0; i < responseLines.length; i++) {
+				responseLines[i] = responseLines[i].split("data: ")[1];
+			}
+
+			console.log(responseLines.filter((line) => line && line.includes("`")).map((line) => JSON.parse(line).choices[0].delta.content));
+
+			const newLine = `\n\n<hr class="__chatgpt_plugin">\n\nrole::assistant\n\n`;
+			editor.replaceRange(newLine, editor.getCursor());
+
+			// move cursor to end of file
+			const cursor = editor.getCursor();
+			const newCursor = {
+				line: cursor.line,
+				ch: cursor.ch + newLine.length
+			}
+			editor.setCursor(newCursor);
+
+			let fullstr = "";
+
+			// loop through response lines
+			for (const responseLine of responseLines) {
+				// if response line is not [DONE] then parse json and append delta to file
+				if (responseLine && !responseLine.includes("[DONE]")) {
+					const responseJSON = JSON.parse(responseLine);
+					const delta = responseJSON.choices[0].delta.content;
+
+					// if delta is not undefined then append delta to file
+					if (delta) {
+						const cursor = editor.getCursor();
+						if (delta === "`") {
+							console.log("single backtick");
+							editor.replaceRange(delta, cursor);
+							// do not move cursor
+							await new Promise(r => setTimeout(r, 82)); // what in the actual fuck -- why does this work 
+
+						} else {
+							editor.replaceRange(delta, cursor);
+							await new Promise(r => setTimeout(r, 28));
+
+						}
+
+						const newCursor = {
+							line: cursor.line,
+							ch: cursor.ch + delta.length
+						}
+						editor.setCursor(newCursor);
+						
+						fullstr += delta;
+
+					}
+				}
+			}
+
+			console.log(fullstr)
+
+			return "streaming";
+		} else {
+			const responseJSON = JSON.parse(response);
+			return responseJSON.choices[0].message.content;
+		}
+	}
+
+	splitMessages(text: string) {
+		// <hr class="__chatgpt_plugin">
+		const messages = text.split('<hr class="__chatgpt_plugin">');
+		return messages;
+	}
+
+	extractRoleAndMessage(message: string) {
+
+		/*
+		extract role from message
+		role::assistant
+
+
+		message content (can be multiple lines)
+		*/
+
+		try {
+			const role = message.split("role::")[1].split("\n")[0];
+			const content = message.split("role::")[1].split("\n").slice(1).join("\n");
+			return {role, content};
+		} catch (err) {
+			throw new Error("Error extracting role and message");
+		}
+
+
+	}
+
+	appendMessage(editor: Editor, role: string, message: string) {
+		/*
+		 append to bottom of editor file:
+		 	const newLine = `${lineBeforeCursor}<hr class="__chatgpt_plugin">\nrole::${role}\n\n${message}`;
+		*/
+
+		const lineBeforeCursor = editor.getLine(editor.getCursor().line);
+		const newLine = `${lineBeforeCursor}\n\n<hr class="__chatgpt_plugin">\n\nrole::${role}\n\n${message}\n\n<hr class="__chatgpt_plugin">\n\nrole::user\n\n`;
+		editor.replaceRange(newLine, editor.getCursor());
+	}
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
 		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
 		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+		statusBarItemEl.setText("Status Bar Text");
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
 		// This adds an editor command that can perform some operation on the current editor instance
 		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
+			id: "call-chatgpt-api",
+			name: "Call ChatGPT API",
 			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
+				const messages = this.splitMessages(editor.getValue());
+				const messagesWithRoleAndMessage = messages.map((message) => {
+					return this.extractRoleAndMessage(message);
+				});
+				this.callOpenAIAPI(editor, messagesWithRoleAndMessage).then((response) => {
+					if (response === "streaming") {
+						// append \n\n<hr class="__chatgpt_plugin">\n\nrole::user\n\n
+						const lineBeforeCursor = editor.getLine(editor.getCursor().line);
+						const newLine = `${lineBeforeCursor}\n\n<hr class="__chatgpt_plugin">\n\nrole::user\n\n`;
+						editor.replaceRange(newLine, editor.getCursor());
+					} else {
+						this.appendMessage(editor, "assistant", response);
 					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
+				});
+			},
 		});
+		
+
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		this.addSettingTab(new ChatGPT_MDSettingsTab(this.app, this));
 	}
 
-	onunload() {
-
-	}
+	onunload() {}
 
 	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+		this.settings = Object.assign(
+			{},
+			DEFAULT_SETTINGS,
+			await this.loadData()
+		);
 	}
 
 	async saveSettings() {
@@ -91,47 +241,34 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
+class ChatGPT_MDSettingsTab extends PluginSettingTab {
+	plugin: ChatGPT_MD;
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: ChatGPT_MD) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
 
 	display(): void {
-		const {containerEl} = this;
+		const { containerEl } = this;
 
 		containerEl.empty();
 
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
+		containerEl.createEl("h2", { text: "Settings for ChatGPT MD:" });
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+			.setName("OpenAI API Key")
+			.setDesc("API Key for OpenAI")
+			.addText((text) =>
+				text
+					.setPlaceholder("some-api-key")
+					.setValue(this.plugin.settings.apiKey)
+					.onChange(async (value) => {
+						this.plugin.settings.apiKey = value;
+						await this.plugin.saveSettings();
+					})
+			);
 	}
 }
