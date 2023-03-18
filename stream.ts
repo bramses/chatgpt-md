4508c639aa778b51f14ca5fe1719d62ff2cb0343
Bram Adams <3282661+bramses@users.noreply.github.com>
2023-03-17 23:51:12 -0400

    cm dispatch

--- stream.ts
+++ stream.ts
@@ -24,23 +24,7 @@ export interface OpenAIStreamPayload {
 
 export const streamSSE = async (editor: Editor, apiKey: string, options: OpenAIStreamPayload) => {
   console.log("streamSSE", options)
-	// const data: OpenAIStreamPayload = {
-	// 	model: "gpt-3.5-turbo",
-	// 	messages: [
-	// 		{
-	// 			role: "system",
-	// 			content: "A conversation between two people.",
-	// 		},
-	// 		{
-	// 			role: "user",
-	// 			content: "Write the game of pong. Write it as a python script",
-	// 		},
-	// 	],
-	// 	temperature: 0.5,
-	// 	presence_penalty: 1,
-	// 	max_tokens: 1000,
-	// 	stream: true, // Enable streaming in response
-	// };
+
 	const url = "https://api.openai.com/v1/chat/completions";
 // Write the game of pong. Write it as a python script
 
@@ -53,6 +37,9 @@ export const streamSSE = async (editor: Editor, apiKey: string, options: OpenAIS
 		payload: JSON.stringify(options),
 	});
 
+  let txt = ""
+  const initialCursorPosCh = editor.getCursor().ch
+  const initialCursorPosLine = editor.getCursor().line
 	source.addEventListener("message", (e: any) => {
 		if (e.data != "[DONE]") {
 			const payload = JSON.parse(e.data);
@@ -63,15 +50,18 @@ export const streamSSE = async (editor: Editor, apiKey: string, options: OpenAIS
 				return;
 			}
 
-			// const cursor = editor.getCursor();
-			// const convPos = editor.posToOffset(cursor);
-			// const cm6 = editor.cm;
-			// const transaction = cm6.state.update({
-			// 	changes: { from: convPos, to: convPos, insert: text },
-			// });
-			// cm6.dispatch(transaction);
 			const cursor = editor.getCursor();
-			editor.replaceRange(text, cursor);
+			const convPos = editor.posToOffset(cursor);
+      
+			const cm6 = editor.cm;
+			const transaction = cm6.state.update({
+				changes: { from: convPos, to: convPos, insert: text },
+			});
+			cm6.dispatch(transaction);
+			// const cursor = editor.getCursor();
+			// editor.replaceRange(text, cursor);
+
+      txt += text
 
 			const newCursor = {
 				line: cursor.line,
@@ -80,6 +70,13 @@ export const streamSSE = async (editor: Editor, apiKey: string, options: OpenAIS
 			editor.setCursor(newCursor);
 		} else {
 			source.close();
+      console.log("txt", txt)
+
+      // replace the text from initialCursor to fix any formatting issues
+      const cursor = editor.getCursor();
+      editor.replaceRange(txt, {line: initialCursorPosLine, ch: initialCursorPosCh}, cursor);
+
+
 		}
 	});
 
@@ -91,229 +88,3 @@ export const streamSSE = async (editor: Editor, apiKey: string, options: OpenAIS
 
 	source.stream();
 };
-
-// attempt 3
-
-// import { IncomingMessage } from "http";
-
-//   export interface OpenAIStreamPayload {
-//     model: string;
-//     messages: Array<{ role: string; content: string }>;
-//     temperature: number;
-//     presence_penalty: number;
-//     max_tokens: number;
-//     stream: boolean;
-//   }
-
-//   export const ChatCompletionRequestMessageRoleEnum = {
-//     System: 'system',
-//     User: 'user',
-//     Assistant: 'assistant'
-// } as const;
-
-// export type ChatCompletionRequestMessageRoleEnum = typeof ChatCompletionRequestMessageRoleEnum[keyof typeof ChatCompletionRequestMessageRoleEnum];
-
-//   export interface ChatCompletionRequestMessage {
-//     /**
-//      * The role of the author of this message.
-//      * @type {string}
-//      * @memberof ChatCompletionRequestMessage
-//      */
-//     'role': ChatCompletionRequestMessageRoleEnum;
-//     /**
-//      * The contents of the message
-//      * @type {string}
-//      * @memberof ChatCompletionRequestMessage
-//      */
-//     'content': string;
-//     /**
-//      * The name of the user in a multi-user chat
-//      * @type {string}
-//      * @memberof ChatCompletionRequestMessage
-//      */
-//     'name'?: string;
-// }
-
-// export interface ChatCompletionResponseMessage {
-//     /**
-//      * The role of the author of this message.
-//      * @type {string}
-//      * @memberof ChatCompletionResponseMessage
-//      */
-//     'role': ChatCompletionResponseMessageRoleEnum;
-//     /**
-//      * The contents of the message
-//      * @type {string}
-//      * @memberof ChatCompletionResponseMessage
-//      */
-//     'content': string;
-// }
-
-// export const ChatCompletionResponseMessageRoleEnum = {
-//     System: 'system',
-//     User: 'user',
-//     Assistant: 'assistant'
-// } as const;
-
-// export type ChatCompletionResponseMessageRoleEnum = typeof ChatCompletionResponseMessageRoleEnum[keyof typeof ChatCompletionResponseMessageRoleEnum];
-
-// export async function* main() {
-// 	try {
-// 		const payload: OpenAIStreamPayload = {
-// 			model: "gpt-3.5-turbo",
-// 			messages: [
-// 				{
-// 					role: "system",
-// 					content: "A conversation between two people.",
-// 				},
-// 				{ role: "user", content: "What is the meaning of life?" },
-// 			],
-// 			temperature: 0.5,
-// 			presence_penalty: 1,
-// 			max_tokens: 100,
-// 			stream: true, // Enable streaming in response
-// 		};
-// 		const result = await fetch("https://api.openai.com/v1/chat/completions", {
-// 			headers: {
-// 				"Content-Type": "application/json",
-// 				Authorization: `Bearer`, //< DO NOT COMMIT
-// 			},
-// 			method: "POST",
-// 			body: JSON.stringify(payload),
-// 		});
-
-//         const text = await result.text();
-
-// 		const stream = text as any as IncomingMessage;
-
-//         console.log ("stream", stream)
-// 		for await (const chunk of stream) {
-//             console.log("chunk", chunk);
-// 			const line = chunk.toString().trim();
-// 			const message = line.split("data: ")[1];
-
-// 			if (message === "[DONE]") {
-// 				break;
-// 			}
-
-// 			const data = JSON.parse(message) as any;
-
-// 			yield data.choices[0].delta.content;
-// 		}
-// 	} catch (error) {
-// 		// if (fallback) yield fallback;
-// 		console.error(error);
-// 	}
-// }
-
-// function onParse(event: ParsedEvent | ReconnectInterval) {
-// 	if (event.type === "event") {
-// 		console.log("Received event!");
-// 		console.log("id:%s", event.id || "<none>");
-// 		console.log("name:%s", event.type || "<none>");
-// 		console.log("data:%s", event.data);
-// 	} else if (event.type === "reconnect-interval") {
-// 		console.log(
-// 			"We should set reconnect interval to %d milliseconds",
-// 			event.value
-// 		);
-// 	}
-// }
-
-// export const main = async () => {
-// 	const res = await fetch("https://api.openai.com/v1/chat/completions", {
-// 		headers: {
-// 			"Content-Type": "application/json",
-// 			Authorization: `Bearer`, //< DO NOT COMMIT
-// 		},
-// 		method: "POST",
-// 		body: JSON.stringify(payload),
-// 	});
-
-// 	//Create Parser Instance.
-// 	const parser = createParser(onParse);
-
-// 	for await (const chunk of res.body as any) {
-// 		parser.feed(chunk);
-// 	}
-
-// 	parser.reset();
-// 	console.log("Done!");
-// };
-
-// ...
-
-//   export interface OpenAIStreamPayload {
-//     model: string;
-//     messages: Array<{ role: string; content: string }>;
-//     temperature: number;
-//     presence_penalty: number;
-//     max_tokens: number;
-//     stream: boolean;
-//   }
-
-//   export async function OpenAIStream(payload: OpenAIStreamPayload) {
-
-//     const encoder = new TextEncoder();
-//     const decoder = new TextDecoder();
-
-//     console.log("OpenAIStream", payload);
-
-//     const res = await fetch("https://api.openai.com/v1/chat/completions", {
-//       headers: {
-//         "Content-Type": "application/json",
-//         Authorization: `Bearer `, // < DO NOT COMMIT
-//       },
-//       method: "POST",
-//       body: JSON.stringify(payload),
-//     });
-
-//     const stream = new ReadableStream({
-//       async start(controller) {
-//         // callback
-//         function onParse(event: ParsedEvent | ReconnectInterval) {
-//             console.log("onParse", event);
-//           if (event.type === "event") {
-//             const data = event.data;
-//             if (data === "[DONE]") {
-//               controller.close();
-//               return;
-//             }
-//             try {
-//               const json = JSON.parse(data);
-//               const text = json.choices[0].delta.content;
-//               const queue = encoder.encode(text);
-//               controller.enqueue(queue);
-//               console.log("onParse", text);
-//             } catch (e) {
-//               // maybe parse error
-//               controller.error(e);
-//             }
-//           }
-//         }
-
-//         // stream response (SSE) from OpenAI may be fragmented into multiple chunks
-//         // this ensures we properly read chunks and invoke an event for each SSE event stream
-//         const parser = createParser(onParse);
-//         // https://web.dev/streams/#asynchronous-iteration
-//         for await (const chunk of res.body as any) {
-//           parser.feed(decoder.decode(chunk));
-//         }
-//       },
-//     });
-
-//     return stream;
-//   }
-
-// export const main = async () => {
-//     const payload: OpenAIStreamPayload = {
-//         model: "gpt-3.5-turbo",
-//         messages: [{ role: "user", content: "Write a poem about Abe Lincoln" }],
-//         temperature: 0.9,
-//         presence_penalty: 0.6,
-//         max_tokens: 100,
-//         stream: true,
-//       };
-//       const stream = await OpenAIStream(payload);
-//       return new Response(stream);
-//     }
