a26760a49c7e5c0b1168d13c303c3ed12840281b
Bram Adams <3282661+bramses@users.noreply.github.com>
2023-03-18 02:09:30 -0400

    handling extra text from speedy stream

--- stream.ts
+++ stream.ts
@@ -4,29 +4,34 @@
 // 	ReconnectInterval,
 // } from "eventsource-parser";
 
-import { Editor } from "obsidian";
+import { Editor, Notice } from "obsidian";
 import { SSE } from "sse";
 
 export interface OpenAIStreamPayload {
 	model: string;
 	messages: Array<{ role: string; content: string }>;
 	temperature: number;
-  top_p: number;
+	top_p: number;
 	presence_penalty: number;
-  frequency_penalty: number;
-  stop: string[] | null;
-  n: number;
-  logit_bias?: any | null;
-  user?: string | null;
+	frequency_penalty: number;
+	stop: string[] | null;
+	n: number;
+	logit_bias?: any | null;
+	user?: string | null;
 	max_tokens: number;
 	stream: boolean;
 }
 
-export const streamSSE = async (editor: Editor, apiKey: string, options: OpenAIStreamPayload) => {
-  console.log("streamSSE", options)
+// todo: await this function and return the text on stream completion
+export const streamSSE = async (
+	editor: Editor,
+	apiKey: string,
+	options: OpenAIStreamPayload,
+	setAtCursor: boolean
+) => {
+	console.log("streamSSE", options);
 
 	const url = "https://api.openai.com/v1/chat/completions";
-// Write the game of pong. Write it as a python script
 
 	const source = new SSE(url, {
 		headers: {
@@ -37,9 +42,9 @@ export const streamSSE = async (editor: Editor, apiKey: string, options: OpenAIS
 		payload: JSON.stringify(options),
 	});
 
-  let txt = ""
-  const initialCursorPosCh = editor.getCursor().ch
-  const initialCursorPosLine = editor.getCursor().line
+	let txt = "";
+	const initialCursorPosCh = editor.getCursor().ch;
+	const initialCursorPosLine = editor.getCursor().line;
 	source.addEventListener("message", (e: any) => {
 		if (e.data != "[DONE]") {
 			const payload = JSON.parse(e.data);
@@ -52,16 +57,15 @@ export const streamSSE = async (editor: Editor, apiKey: string, options: OpenAIS
 
 			const cursor = editor.getCursor();
 			const convPos = editor.posToOffset(cursor);
-      
+
 			const cm6 = editor.cm;
 			const transaction = cm6.state.update({
 				changes: { from: convPos, to: convPos, insert: text },
 			});
 			cm6.dispatch(transaction);
-			// const cursor = editor.getCursor();
-			// editor.replaceRange(text, cursor);
 
-      txt += text
+
+			txt += text;
 
 			const newCursor = {
 				line: cursor.line,
@@ -70,13 +74,42 @@ export const streamSSE = async (editor: Editor, apiKey: string, options: OpenAIS
 			editor.setCursor(newCursor);
 		} else {
 			source.close();
-      console.log("txt", txt)
+			console.log("txt", txt);
 
-      // replace the text from initialCursor to fix any formatting issues
-      const cursor = editor.getCursor();
-      editor.replaceRange(txt, {line: initialCursorPosLine, ch: initialCursorPosCh}, cursor);
+			// replace the text from initialCursor to fix any formatting issues from streaming
+			const cursor = editor.getCursor();
+			editor.replaceRange(
+				txt,
+				{ line: initialCursorPosLine, ch: initialCursorPosCh },
+				cursor
+			);
 
+			// set cursor to end of replacement text
+			const newCursor = {
+				line: initialCursorPosLine,
+				ch: initialCursorPosCh + txt.length,
+			};
+			editor.setCursor(newCursor);
 
+			// remove extraneous text
+			// const textAfterCursor = editor.getRange(newCursor, {
+			// 	line: Infinity,
+			// 	ch: Infinity,
+			// });
+
+			if (!setAtCursor) {
+				// remove the text after the cursor
+				editor.replaceRange("", newCursor, {
+					line: Infinity,
+					ch: Infinity,
+				});
+			} else {
+				new Notice(
+					"[ChatGPT MD] Text pasted at cursor may leave artifacts. Please remove them manually. ChatGPT MD cannot safely remove text when pasting at cursor."
+				);
+			}
+
+      return txt;
 		}
 	});
 
